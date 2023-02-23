c5be2478b48bb26e9e1099faf93248f89d7624dd
Daniel Febrero <febrero@me.com>
2023-02-23 12:23:50 +0100

    Implement clickOutside.js mixin

---
+++ src/mixins/clickOutside.js
@@ -0,0 +1,16 @@
+export default {
+    mounted() {
+        document.addEventListener('click', this.handleClickOutside);
+    },
+    beforeDestroy() {
+        document.removeEventListener('click', this.handleClickOutside);
+    },
+    methods: {
+        handleClickOutside(event) {
+            const element = this.$el;
+            if (!element.contains(event.target)) {
+                this.onClickOutside(event);
+            }
+        }
+    }
+};
