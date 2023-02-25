25e50cda2949848e2878c211347b729cd33e40d1
Daniel Febrero <febrero@me.com>
2023-02-25 10:35:45 +0100

    Fix utils imports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,10 @@
 import * as ajax from "./ajax";
 import * as date from "./date";
-import * as dom from "./date";
-import * as log from "./date";
-import * as number from "./date";
-import * as url from "./date";
-import * as utils from "./date";
+import * as dom from "./dom";
+import * as log from "./log";
+import * as number from "./number";
+import * as url from "./url";
+import * as type from "./type";
 
 import { UtilsConfig } from "../types/utils";
 import { Plugin } from "../types/core";
@@ -19,7 +19,7 @@ export const createUtils = (config: UtilsConfig): Plugin => {
         log,
         number,
         url,
-        utils,
+        type,
       }),
   };
 }
