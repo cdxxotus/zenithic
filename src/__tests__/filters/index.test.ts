e334efd808b517e04ae8c2a9731894a4afff0d26
Daniel Febrero <febrero@me.com>
2023-02-28 10:03:55 +0100

    Add tests/filters/index

---
+++ src/__tests__/filters/index.test.ts
@@ -0,0 +1,42 @@
+const { createZenithic } = require('"../../../src');
+const { createFilters } = require("../../filters");
+const defaultConfig = require("../../config");
+
+let app;
+let filters;
+
+beforeAll(() => {
+    filters = createFilters();
+});
+
+beforeEach(() => {
+  app = null;
+});
+
+test("createFilters", () => {
+  expect(typeof filters).toBe("object");
+  expect(Object.keys(filters).includes("install")).toBeTruthy();
+  expect(Object.keys(filters).length).toBe(1);
+});
+
+test("app.use(filters) with no filters in app.config or createFilters.config", () => {
+  app = createZenithic({ filters: [] });
+  expect(app.filters).toStrictEqual({});
+
+  app.use(filters);
+  expect(app.filters).toStrictEqual({});
+});
+
+test("app.use(filters) with no filters in app.config but in createFilters.config", () => {
+  app = createZenithic({ filters: [] });
+  expect(app.filters).toStrictEqual({});
+
+  filters = createFilters(["date", "lowercase"]);
+  app.use(filters);
+  expect(Object.keys(app.filters).sort().toString()).toBe("date,lowercase");
+});
+
+test("app.use(filters) with filters in app.config", () => {
+  app = createZenithic();
+  expect(Object.keys(app.filters).sort().toString()).toBe(defaultConfig.default.filters.sort().toString());
+});
