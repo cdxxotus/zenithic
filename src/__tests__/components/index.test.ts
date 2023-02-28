5c585b666e26af27cae0a0677b8a9f5037816bfe
Daniel Febrero <febrero@me.com>
2023-02-28 10:07:35 +0100

    Add tests/components/index

---
+++ src/__tests__/components/index.test.ts
@@ -0,0 +1,42 @@
+const { createZenithic } = require('"../../../src');
+const { createComponents } = require("../../components");
+const defaultConfig = require("../../config");
+
+let app;
+let components;
+
+beforeAll(() => {
+    components = createComponents();
+});
+
+beforeEach(() => {
+  app = null;
+});
+
+test("createComponents", () => {
+  expect(typeof components).toBe("object");
+  expect(Object.keys(components).includes("install")).toBeTruthy();
+  expect(Object.keys(components).length).toBe(1);
+});
+
+test("app.use(components) with no components in app.config or createComponents.config", () => {
+  app = createZenithic({ components: [] });
+  expect(app.components).toStrictEqual({});
+
+  app.use(components);
+  expect(app.components).toStrictEqual({});
+});
+
+test("app.use(components) with no components in app.config but in createComponents.config", () => {
+  app = createZenithic({ components: [] });
+  expect(app.components).toStrictEqual({});
+
+  components = createComponents(["Button", "Input"]);
+  app.use(components);
+  expect(Object.keys(app.components).sort().toString()).toBe("Button,Input");
+});
+
+test("app.use(components) with components in app.config", () => {
+  app = createZenithic();
+  expect(Object.keys(app.components).sort().toString()).toBe(defaultConfig.default.components.sort().toString());
+});
