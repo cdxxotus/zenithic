6bd7633f2407cce213c1f90ee23feba7543f9421
Daniel Febrero <febrero@me.com>
2023-03-04 16:42:03 +0100

    Remove non sense

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,8 +4,4 @@ import { RouterConfig, Route } from "../types/router";
  * @param config Router configuration
  * @returns {Route[]}
  */
-export const createRoutes = (config?: RouterConfig) => {
-  const routes: Route[] = [].concat(...config?.routes ?? []);
-
-  return routes;
-};
+export const createRoutes = (config?: RouterConfig) => config?.routes ?? [];
